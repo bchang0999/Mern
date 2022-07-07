706270b43fdfd022ff6f5516bd84e98fdab5b0d9
Brandon Chang <bchang0999@gmail.com>
2022-07-07 18:54:27 -0400

    First JSX

---
+++ Practice/first-app-pro/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello Dojo!
+      </h1>
+      <h3>
+        Things I need to do:
+      </h3>
+      <h5>
+        <ul>
+          <li>Learn to code</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Pass this course</li>
+        </ul>
+      </h5>
+    </div>
+  );
+}
+
+export default App;
